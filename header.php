1608972d31b1a46ceae9f9ef13f6bebe68752c5d
erickahannah <130682255+erckka@users.noreply.github.com>
2023-11-13 22:42:32 +0800

    Update and rename header.html to header.php

---
+++ header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suero General Hospital</title>
+    <!-- CSS Bootstrap Link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    <!-- Font-awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" type="text/css" integrity="sha512-PgZKc0lCwo0sahsMcSQ9X9y0uzCyXLn1tGv/Jt+ZUCNqqMf2QsCnB+PkiHJpDdC1fBey+8Es1mZYfRZexmLJqw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Page Icon -->
+    <link rel="shortcut icon" href="images/Logo.png">
+    <!-- Bootstrap JS and Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js" integrity="sha512-SkkgT5pfwPhcDN7T+8CpSv96mrQtrlY5eFZI1PcZ6BD+KhtViZaOd57tK4gD1UryqK2Pq2PKXTNPo/WZtk6Ckw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+
+    <body>
+    <div style="height: 50px; background-color: #fff;"></div>
+
+    <nav class="navbar navbar-expand-md navbar-light bg-light" style="height: 70px; background: #E6ECE2;">
+        <!-- Navbar Start -->
+        <div class="container-fluid">
+            <!-- Logo and Hospital Name -->
+            <a class="navbar-brand" href="index.php">
+                <img src="images/logo.png" width="50px" height="50px" class="ml-2" alt="Logo">
+                <span style="color: #1B4303; font-size: 30px; font-weight: bold ; font-family: League Gothic; margin-left: 10px">SUERO GENERAL HOSPITAL</span>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav text-md-right ms-auto">
+                    <a class="nav-link mx-2" href="services.php"
+                        style="color: #1B4303; font-size: 22px; font-family: Lekton; font-weight: 700; word-wrap: break-word;">Services</a>
+                    <a class="nav-link mx-2" href="#"
+                        style="color: #1B4303; font-size: 22px; font-family: Lekton; font-weight: 700; word-wrap: break-word;">Appointment</a>
+                    <a class="nav-link mx-2" href="#"
+                        style="color: #1B4303; font-size: 22px; font-family: Lekton; font-weight: 700; word-wrap: break-word;">Doctor</a>
+                    <a class="nav-link mx-2" href="#"
+                        style="color: #1B4303; font-size: 22px; font-family: Lekton; font-weight: 700; word-wrap: break-word;">About Us</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</body>
+
+
+
+
+
+
+
+</html>
+
+
+

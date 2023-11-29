e3296d0bbb71d1fe797230080cef165631283cd3
erickahannah <130682255+erckka@users.noreply.github.com>
2023-11-29 17:41:41 +0800

    Update footer.html
    No need for footer css

--- footer.php
+++ footer.php
@@ -1,58 +1,66 @@
-//FOOTER
 <!DOCTYPE html>
 <html>
-    <head>
-        <!--FOOTER CSS-->
-        <link rel="stylesheet" href="footer.css">
-    </head>
-    <body>
-        <!-- <div style="height: 100px;" class="footer_bg"> -->
-            
-        </div>
-    </body>
-    <footer>
-        <div class="footer_bg">
-            <table>
-                <tr class="row_1">
-                    <th class="footer_left"></th>
-                    <th class="footer_mid"></th>
-                    <th class="footer_right"></th>
-                </tr>
-                <tr class="row_2">
-                    <td class="logo_name">
-                        <img src="images/logo.png" alt="logo.png" id="logo">
-                        <h3 class="sgh">SUERO GENERAL HOSPITAL</h3>
-                    </td>
-                    <td rowspan="3" class="footer_mid_cell">
-                        <table class="footer_mid_table">
-                            <tr><td class="services">SERVICES</td></tr>
-                            <tr><td class="doctor">DOCTOR</td></tr>
-                            <tr><td class="about_us">ABOUT US</td></tr>    
-                            <tr><td class="appointment">APPOINTMENT</td></tr> 
-                        </table>
-                    </td>
-                    <td>CONTACT US:</td>
-                    
+  <head>
+    <!--FOOTER CSS-->
+    <!-- CSS Bootstrap Link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <!-- Font-awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" type="text/css" integrity="sha512-PgZKc0lCwo0sahsMcSQ9X9y0uzCyXLn1tGv/Jt+ZUCNqqMf2QsCnB+PkiHJpDdC1fBey+8Es1mZYfRZexmLJqw=="  crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  </head>
+  <body>
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-body-tertiary text-muted">
+      <!-- Section: Links  -->
+      <section class="mt-5">
+        <div class="container text-md-start">
+          <!-- Grid row -->
+          <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 mx-auto my-3">
+              <!-- Content -->
+              <img src="images/logo.png" width="30px" height="30px">
+              <span style="color: #1B4303; font-size: 25px; font-weight: bold; font-family: League Gothic; margin-left: 2px">SUERO GENERAL HOSPITAL</span>
+              <br> <br>
+              <h5> Suero General Hospital, QFV3+8QQ, Cabugao, Ilocos Sur </h5>
+            </div>
+            <!-- Grid column -->
 
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 my-3">
+              <!-- Links -->
+              <a href="services.html" style="text-decoration:underline; color: #1B4303;">
+                <h6 class="text-uppercase fw-bold mb-4" style="color: #1B4303; font-size: 20px; font-weight: bold; font-family: League Gothic;">Services</h6>
+              </a>
+              <a href="appointment.js" style="text-decoration:underline; color: #1B4303;">
+                <h6 class="text-uppercase fw-bold mb-4" style="color: #1B4303; font-size: 20px; font-weight: bold; font-family: League Gothic;">Appointment</h6>
+              </a>
+              <a href="doctor.html" style="text-decoration:underline; color: #1B4303;">
+                <h6 class="text-uppercase fw-bold mb-4" style="color: #1B4303; font-size: 20px; font-weight: bold; font-family: League Gothic;">Doctor</h6>
+              </a>
+              <a href="aboutus.html" style="text-decoration:underline; color: #1B4303;">
+                <h6 class="text-uppercase fw-bold mb-4" style="color: #1B4303; font-size: 20px; font-weight: bold; font-family: League Gothic;">About Us</h6>
+              </a>
+            </div>
+            <!-- Grid column -->
 
-                    
-                    <!-- <td>Contact Us:</td> -->
-                </tr>
-                <tr class="row_3">
-                    <tr>
-                        <td rowspan="4;" class="address">Address Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                            Excepteur sint occaecat cupidatat non proident, 
-                            sunt in culpa qui officia deserunt mollit anim id est laborum
-                        </td>
-                        <td class="icons">
-                            <img src="images/fb.png" alt="fb.png" id="fb">
-                            <img src="images/viber.png" alt="viber.png" id="viber">
-                            <img src="images/ig.png" alt="ig.png" id="ig">
-                        </td>
-                    </tr>
-                </tr>    
-            </table>
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto my-3">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4" style="color: #1B4303; font-size: 20px; font-weight: bold; font-family: League Gothic;">Contact Us</h6>
+              <p>
+                <i class="fas fa-envelope me-3"></i>
+                suerogeneralhospital@gmail.com
+              </p>
+              <p><i class="fas fa-phone me-3"></i> 09123456158</p>
+            </div>
+          </div>
         </div>
+      </section>
+      <!-- Copyright -->
+      <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        © Dela Cruz, Evangelista, Teng
+      </div>
     </footer>
+  </body>
 </html>

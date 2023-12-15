5048dbd12502cf00b4694d0797eb0c2863ec3c5e
all79 <130957740+all79@users.noreply.github.com>
2023-12-15 18:42:49 +0800

    server.js

--- javascript/server.js
+++ javascript/server.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const cors = require("cors");
 
 const app = express();
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/SGH', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
+app.use(express.json());
+app.use(express.static(__dirname));
+
+mongoose.connect("mongodb://127.0.0.1:27017/SGH", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const PatientSchema = mongoose.Schema({
   lname: String,
@@ -31,37 +32,45 @@ const PatientSchema = mongoose.Schema({
   pulmosched: String,
   ultrasched: String,
   xraysched: String,
-  ctsched: String
+  ctsched: String,
 });
 
-const UserModel = mongoose.model("patient", PatientSchema);
+const UserModel = mongoose.model("patients", PatientSchema);
+
+app.get("/", (req, res) => {
+  console.log("Path request received");
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.get("/appointment.html", (req, res) => {
-  console.log("Received Appointment API Request")
-  res.sendFile(path.join(__dirname, '/appointment.html'));
+  console.log("Received Appointment API Request");
+  res.sendFile(path.join(__dirname, "/appointment.html"));
 });
 
 app.get("/about_us.html", (req, res) => {
-  console.log("Received About Us API Request")
-  res.sendFile(path.join(__dirname, '/about_us.html'));
+  console.log("Received About Us API Request");
+  res.sendFile(path.join(__dirname, "/about_us.html"));
 });
 
 app.get("/services.html", (req, res) => {
-  console.log("Received Services API Request")
-  res.sendFile(path.join(__dirname, '/services.html'));
+  console.log("Received Services API Request");
+  res.sendFile(path.join(__dirname, "/services.html"));
 });
 
 app.get("/doctors.html", (req, res) => {
-  console.log("Received Doctors API Request")
-  res.sendFile(path.join(__dirname, '/doctors.html'));
+  console.log("Received Doctors API Request");
+  res.sendFile(path.join(__dirname, "/doctors.html"));
 });
 
-app.get('/index.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
-})
+app.get("/index.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
 
 app.post("/", async (req, res) => {
-  console.log('Received Post Request')
+  console.log("Received Post Request");
+
+  const patientData = req.body;
+
   try {
     const {
       lname,
@@ -104,6 +113,8 @@ app.post("/", async (req, res) => {
     });
 
     await newUser.save();
+
+    //res.status(200).send("Data saved successfully");
 
     // Insert Modal
   } catch (error) {
